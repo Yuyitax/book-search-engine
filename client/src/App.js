431101ad2f2a6bd7ca1dca84f776344a359f2aed
Cristina <cristinayunesm@gmail.com>
2022-12-08 22:40:17 -0500

    fixing issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -9,20 +9,20 @@ function App() {
     <Router>
       <>
         <Navbar />
-        <Routes>
+        <Switch>
           <Route 
             path='/' 
-            element={<SearchBooks />} 
+            component={ SearchBooks } 
           />
           <Route 
             path='/saved' 
-            element={<SavedBooks />} 
+            component={ SavedBooks } 
           />
           <Route 
             path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
+            component={<h1 className='display-2'>Wrong page!</h1>}
           />
-        </Routes>
+        </Switch>
       </>
     </Router>
   );
